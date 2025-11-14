120279e9d080a8dc0a0ad4389826f6d0fda98bd9
ecparadise <ecp.3299@gmail.com>
2025-11-14 16:57:47 -0500

    adding more testimony

--- app/constants/testimony.ts
+++ app/constants/testimony.ts
@@ -9,4 +9,19 @@ export const customerTestimony = [
     feedback:
       "Abbey was able to assess my style and offered me great options. She was generous with her time, creative with her suggestions, energetic, and congenial. Highly recommend her services.",
   },
+  {
+    name: "Valli B.",
+    feedback:
+      "Abbey!!! She helped me pick out my Mother of the Groom dress... I felt so comfortable in my dress all night AND I received many compliments. Abbey was fabulous, I would definitely seek out her expertise again.",
+  },
+  {
+    name: "Anonymous",
+    feedback:
+      "Abbey is great!  I needed an outfit for an evening event and she helped me put together a silk tuxedo with shoes, purse and jewelry. The next time I saw Abbey she guided me on selecting a great sneaker for “everyday.” She’s a great resource and I will use her again. (Plus she’s fun to work with!)",
+  },
+  {
+    name: "Anonymous",
+    feedback:
+      "What can I say… Abbey brings confidence and style that has made me comfortable for many years. She brings a thoughtful approach and understands my unique preferences and personality. I always feel great working with Abbey. And I look good too. Thanks!!!",
+  },
 ];
