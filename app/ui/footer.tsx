edf2db0a3d7c74eeedcf5e5e6a8b27210ad9d312
ecparadise <ecp.3299@gmail.com>
2025-11-20 16:59:44 -0500

    adding email on bottom

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -1,20 +1,29 @@
 "use client"
-import { FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaRegEnvelope } from "react-icons/fa";
 
 
 export default function Footer() {
   return (
-    <div className="flex gap-2 justify-center items-center p-4 border-t mt-8 bg-neutral-950 text-white fixed bottom-0 w-full">
+    <div className="flex gap-6 justify-center items-center p-4 border-t mt-8 bg-neutral-950 text-white fixed bottom-0 w-full">
       <a
         href="https://www.instagram.com/oneofakindstyling"
         target="_blank"
         rel="noopener noreferrer"
-        className="flex items-center gap-2"
+        className="flex flex-col sm:flex-row items-center gap-2"
       >
         <FaInstagram size={26} />
-        <p>@oneofakindstyling</p>
+        <p className={"text-sm sm:text-base"}>@oneofakindstyling</p>
       </a>
-    </div>
+      <a
+        href="mailto:oneofakindstyling@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex flex-col sm:flex-row items-center gap-2"
+      >
+        <FaRegEnvelope size={26} />
+        <p className="text-sm sm:text-base">oneofakindstyling@gmail.com</p>
+      </a>
+    </div >
   )
 
 }
