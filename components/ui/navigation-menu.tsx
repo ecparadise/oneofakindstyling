934cf2043eeda3750b40a16bdf96827cbb751f57
ecparadise <ecp.3299@gmail.com>
2026-08-04 17:12:41 -0400

    fixing build error

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -59,12 +59,7 @@ function NavigationMenuItem({
 }
 
 const navigationMenuTriggerStyle = cva(
-  "group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-normal text-black disabled:pointer-events-none disabled:opacity-50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1",
-  {
-    defaultVariants: {
-      variant: "default"
-    }
-  }
+  "group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-normal text-black disabled:pointer-events-none disabled:opacity-50 focus-visible:ring-ring/50 outline-none transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1"
 )
 
 function NavigationMenuTrigger({
